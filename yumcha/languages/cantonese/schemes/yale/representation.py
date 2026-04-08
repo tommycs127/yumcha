ac70c05cf4434b9f78e59034286fba6e09dea093
Wadadon <tommyyung1@hotmail.com>
2026-04-08 17:38:48 +0800

    feat&fix(yumcha.languages.cantonese): add old phonemes to Cantonese phonology

--- yumcha/languages/cantonese/schemes/yale/representation.py
+++ yumcha/languages/cantonese/schemes/yale/representation.py
@@ -5,17 +5,22 @@ from yumcha.language.scheme.representation import Representation, ValidationErro
 
 @dataclass(frozen=True)
 class YaleRepresentation(Representation):
-    REQUIRED = ("nucleus_before_tone", "nucleus_after_tone", "tone", "tone_h")
+    REQUIRED = (
+        "nucleus_before_tone_diacritic",
+        "nucleus_after_tone_diacritic",
+        "tone_diacritic",
+        "tone_h",
+    )
     initial: str
-    nucleus_before_tone: str
-    tone: str
-    nucleus_after_tone: str
+    nucleus_before_tone_diacritic: str
+    tone_diacritic: str
+    nucleus_after_tone_diacritic: str
     coda_vowel: str
     tone_h: str
     coda_consonant: str
 
     def validate(self) -> None:
-        nucleus = self.nucleus_before_tone + self.nucleus_after_tone
+        nucleus = self.nucleus_before_tone_diacritic + self.nucleus_after_tone_diacritic
         coda = self.coda_vowel or self.coda_consonant
 
         invaild_nucleus_coda_comb = {
